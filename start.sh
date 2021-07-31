d64cd16b2c24218be0b03e1865a8c2e5f3f90b19
Алексей Апельчугин <al.apelt@gmail.com>
2021-07-31 23:59:15 +0300

    final 0

--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-
-set -e
-set -u
-
-echo "Migration Django"
-
-python manage.py makemigrations users --no-input
-python manage.py makemigrations reviews --no-input
-python manage.py makemigrations titles --no-input
-python manage.py migrate --noinput
-python manage.py collectstatic --no-input
-python manage.py loaddata  fixtures.json --no-input
-gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
-
-exec "$@"
+sudo python manage.py makemigrations users --no-input
+sudo python manage.py makemigrations reviews --no-input
+sudo python manage.py makemigrations titles --no-input
+sudo python manage.py migrate --noinput
+sudo python manage.py collectstatic --no-input
+sudo python manage.py loaddata  fixtures.json --no-input
+sudo gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000

91a47fca1a8294f29f181e4ed9863d2f686e07dc
Алексей Апельчугин <al.apelt@gmail.com>
2021-07-31 23:06:04 +0300

    final

--- migrate.sh
+++ migrate.sh
@@ -8,5 +8,5 @@ python manage.py makemigrations reviews --no-input
 python manage.py makemigrations titles --no-input
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input
-
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
 exec "$@"

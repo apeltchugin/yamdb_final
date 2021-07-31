47fd780d5f60abbf15da3f0412a80b68deb51ff0
Алексей Апельчугин <al.apelt@gmail.com>
2021-07-31 23:01:04 +0300

    final

--- migrate.sh
+++ migrate.sh
@@ -8,6 +8,5 @@ python manage.py makemigrations reviews --no-input
 python manage.py makemigrations titles --no-input
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input
-python manage.py loaddata  fixtures.json --no-input
 
 exec "$@"

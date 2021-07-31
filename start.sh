52c6520b9fe841c2052fe0ab0a70a8b5d6f9d47f
Алексей Апельчугин <al.apelt@gmail.com>
2021-07-31 23:51:04 +0300

    final

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo "Migration Django"
+
+python manage.py makemigrations users --no-input
+python manage.py makemigrations reviews --no-input
+python manage.py makemigrations titles --no-input
+python manage.py migrate --noinput
+python manage.py collectstatic --no-input
+python manage.py loaddata  fixtures.json --no-input
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+
+exec "$@"

36198f9e81695ab6eff70af52a599ed7743cab50
Алексей Апельчугин <al.apelt@gmail.com>
2021-07-31 22:52:28 +0300

    final

--- django_entrypoint.sh
+++ django_entrypoint.sh
@@ -10,6 +10,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --no-input
 python manage.py loaddata  fixtures.json --no-input
 
-gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
-
 exec "$@"
